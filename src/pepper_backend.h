c9995454c52ee5f940e54d54a7a9d78a989d6d98
Maximillian Kirsch <maximillian.kirsch@alumni.fh-aachen.de>
2019-01-30 17:46:00 +0100

    backend is capable to receive ros action outcome in callback; callback sets gpp action-state to corresponding ros action-state outcome

--- src/pepper_backend.h
+++ src/pepper_backend.h
@@ -28,36 +28,36 @@ public:
 	virtual ~Pepper_Backend() override;
 	virtual void execute_activity(shared_ptr<Activity> a)  override;
 	virtual void preempt_activity(shared_ptr<Transition> trans) override;
+	virtual Clock::time_point time() const noexcept override;
 
 	template < typename ActionT > void execute_transition_wrapper(typename ActionT::_action_goal_type::_goal_type &goal, shared_ptr<Activity> a) {
 
 		actionlib::SimpleActionClient < ActionT > &client = std::get < actionlib::SimpleActionClient < ActionT > &>(action_clients);
-		if (true) {
-			client.sendGoalAndWait(goal, ros::Duration(30.0));
-			ROS_INFO("Sending goal");
-			auto state = client.getState();
-			ROS_INFO ("Action finished: %s", state.toString().c_str());
-
-		} else {
-			client.sendGoal(goal, boost::bind(
-				&Pepper_Backend::doneCb<typename ActionT::_action_result_type::_result_type::ConstPtr>,
-				this, a, _1, _2
-			) );
-		}
+		ROS_INFO("Sending goal");
+		client.sendGoal(goal, boost::bind(
+			&Pepper_Backend::doneCb<typename ActionT::_action_result_type::_result_type::ConstPtr>,
+			this, a, _1, _2
+		) );
 	}
 
 	template < typename ResultConstPtrT > void doneCb(shared_ptr<Activity> a, const actionlib::SimpleClientGoalState &state, ResultConstPtrT result) {
 
 		ROS_INFO("Finished in state [%s]", state.toString().c_str());
 		switch(state.state_){
-			case actionlib::SimpleClientGoalState::SUCCEEDED:  update_activity(a->transition(Transition::Hook::FINISH)) ; break;
-			case actionlib::SimpleClientGoalState::PREEMPTED:; break;
-			case actionlib::SimpleClientGoalState::ABORTED:    update_activity(a->transition(Transition::Hook::FAIL)); break;
-			case actionlib::SimpleClientGoalState::PENDING:	 ; break;
-			case actionlib::SimpleClientGoalState::ACTIVE:   ; break;
-			case actionlib::SimpleClientGoalState::RECALLED: ; break;
-			case actionlib::SimpleClientGoalState::REJECTED: ; break;
-			case actionlib::SimpleClientGoalState::LOST: ; break;
+		case actionlib::SimpleClientGoalState::SUCCEEDED:
+			update_activity(a->transition(Transition::Hook::FINISH));
+			break;
+		case actionlib::SimpleClientGoalState::PREEMPTED:
+			break;
+		case actionlib::SimpleClientGoalState::ABORTED:
+			update_activity(a->transition(Transition::Hook::FAIL));
+			break;
+		case actionlib::SimpleClientGoalState::PENDING:
+		case actionlib::SimpleClientGoalState::ACTIVE:
+		case actionlib::SimpleClientGoalState::RECALLED:
+		case actionlib::SimpleClientGoalState::REJECTED:
+		case actionlib::SimpleClientGoalState::LOST:
+			;
 		}
 	}
 
@@ -67,8 +67,8 @@ private:
 	actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> move_base_client;
 
 	std::tuple <
-		actionlib::SimpleActionClient<naoqi_actions::NaoQi_animatedSayAction> & ,
-		actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> &
+	actionlib::SimpleActionClient<naoqi_actions::NaoQi_animatedSayAction> & ,
+	actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> &
 	> action_clients;
 
 
