dd93ad2321646811d4211282a15072e371be5021
Maximillian Kirsch <maximillian.kirsch@alumni.fh-aachen.de>
2019-07-29 19:04:32 +0200

    ros_backend.cpp: get the ActionManager by the mapped_name of the activity and execute the ActionManager by calling a base class method which need to be executed for every ActionManger the same way.

--- src/ros_backend.cpp
+++ src/ros_backend.cpp
@@ -24,8 +24,13 @@ void RosBackend::execute_activity(gpp::shared_ptr<gpp::Activity> a)
 	// TODO: Find AbstractActionManager for a->mapped_name()
 	//       and execute it, e.g.:
 	//       action_manager.execute(a);
+	get_action_client(a).execute(a);
 }
 
+AbstractActionManager& RosBackend::get_action_client(gpp::shared_ptr<gpp::Activity> a)
+{
+	return *action_managers_.find(std::string(a->mapped_name()))->second;
+}
 
 gpp::Clock::time_point RosBackend::time() const noexcept
 {
