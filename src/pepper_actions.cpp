fa241e197a3f93846fc8f6bafbc7f94a9349fe4f
Maximillian Kirsch <maximillian.kirsch@alumni.fh-aachen.de>
2019-07-30 17:14:40 +0200

    pepper_actions.cpp: integrated all available actions

--- src/pepper_actions.cpp
+++ src/pepper_actions.cpp
@@ -52,10 +52,99 @@ ActionManager<naoqi::NaoQi_dialogAction>::build_goal(const gpp::Activity &a)
 	return goal;
 }
 
+template<>
+ActionManager<naoqi::NaoQi_sayAction>::GoalT
+ActionManager<naoqi::NaoQi_sayAction>::build_goal(const gpp::Activity &a)
+{
+	naoqi_wrapper_msgs::NaoQi_sayGoal goal;
+	goal.message.data = std::string(a.mapped_arg_value("say_string"));
+	return goal;
+}
+
+template<>
+ActionManager<naoqi::NaoQi_lookAtAction>::GoalT
+ActionManager<naoqi::NaoQi_lookAtAction>::build_goal(const gpp::Activity &a)
+{
+	naoqi_wrapper_msgs::NaoQi_lookAtGoal goal;
+	goal.position.push_back(double(a.mapped_arg_value("x")));
+	goal.position.push_back(double(a.mapped_arg_value("y")));
+	goal.position.push_back(double(a.mapped_arg_value("z")));
+	goal.frame = int(a.mapped_arg_value("frame"));
+	goal.fractionMaxSpeed = int(a.mapped_arg_value("fractionMaxSpeed"));
+	goal.useWholeBody = bool(a.mapped_arg_value("useWholeBody"));
+	return goal;
+}
+
+template<>
+ActionManager<move_base_msgs::MoveBaseAction>::GoalT
+ActionManager<move_base_msgs::MoveBaseAction>::build_goal(const gpp::Activity &a)
+{
+	move_base_msgs::MoveBaseGoal goal;
+	goal.target_pose.header.frame_id = std::string(a.mapped_arg_value("frame_id"));
+	goal.target_pose.header.stamp =  ros::Time::now();
+	goal.target_pose.pose.position.x = int(a.mapped_arg_value("x"));
+	goal.target_pose.pose.position.y = int(a.mapped_arg_value("y"));
+	goal.target_pose.pose.orientation.w = int(a.mapped_arg_value("w"));;
+	return goal;
+}
+
+template<>
+ActionManager<naoqi::NaoQi_openWebsiteAction>::GoalT
+ActionManager<naoqi::NaoQi_openWebsiteAction>::build_goal(const gpp::Activity &a)
+{
+	naoqi_wrapper_msgs::NaoQi_openWebsiteGoal goal;
+	goal.url.data = std::string(a.mapped_arg_value("url"));
+	goal.waitForWebCommand = bool(a.mapped_arg_value("waitForWebCommand"));
+	return goal;
+}
+
+template<>
+ActionManager<darknet::obj_detectionAction>::GoalT
+ActionManager<darknet::obj_detectionAction>::build_goal(const gpp::Activity &a)
+{
+	darknet_actions_msgs::obj_detectionGoal goal;
+	goal.to_detected_obj = std::string(a.mapped_arg_value("to_detected_obj"));
+	return goal;
+}
+
+template<>
+ActionManager<naoqi::NaoQi_animatedSayAction>::GoalT
+ActionManager<naoqi::NaoQi_animatedSayAction>::build_goal(const gpp::Activity &a)
+{
+	naoqi_wrapper_msgs::NaoQi_animatedSayGoal goal;
+	goal.animatedMessage.data = std::string(a.mapped_arg_value("animatedMessage"));
+	return goal;
+}
+
+template<>
+ActionManager<naoqi::NaoQi_animationAction>::GoalT
+ActionManager<naoqi::NaoQi_animationAction>::build_goal(const gpp::Activity &a)
+{
+	naoqi_wrapper_msgs::NaoQi_animationGoal goal;
+	goal.animation.data = std::string(a.mapped_arg_value("animation"));
+	return goal;
+}
+
+template<>
+ActionManager<naoqi::NaoQi_subscribeAction>::GoalT
+ActionManager<naoqi::NaoQi_subscribeAction>::build_goal(const gpp::Activity &a)
+{
+	naoqi_wrapper_msgs::NaoQi_subscribeGoal goal;
+	goal.eventName.data = std::string(a.mapped_arg_value("eventName"));
+	return goal;
+}
 
 void RosBackend::define_actions()
 {
-	define_action_client<naoqi::NaoQi_dialogAction>("TODO: TOPIC_NAME");
+	define_action_client<naoqi::NaoQi_dialogAction>("/naoqi_dialog_server");
+	define_action_client<naoqi::NaoQi_sayAction>("/naoqi_say_server/naoqi_say");
+	define_action_client<naoqi::NaoQi_lookAtAction>("/naoqi_lookAt_server/lookAt");
+	define_action_client<move_base_msgs::MoveBaseAction>("move_base");
+	define_action_client<naoqi::NaoQi_openWebsiteAction>("/naoqi_openWebsite_server/openWebsite");
+	define_action_client<darknet::obj_detectionAction>("/yolo_obj_detection_position_server");
+	define_action_client<naoqi::NaoQi_animatedSayAction>("/naoqi_animatedSay_server/animatedSay");
+	define_action_client<naoqi::NaoQi_animationAction>("/naoqi_animation_server/naoqi_animation");
+	define_action_client<naoqi::NaoQi_subscribeAction>("/naoqi_subscribe_server/subscribe");
 }
 
 
